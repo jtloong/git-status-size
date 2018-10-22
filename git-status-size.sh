834a0080296b90962a778dae5bb7e426090ee6ea
jtloong <joshua.t.loong@gmail.com>
2018-10-22 19:32:30 -0400

    Added ability to read git status -s

--- git-status-size.sh
+++ git-status-size.sh
@@ -1,1 +1,11 @@
 #!/bin/bash
+
+mapfile -t git_status_files < <( git status -s )
+
+tLen=${#git_status_files[@]}
+
+for (( i=0; i<${tLen}; i++ ));
+do
+  if
+  echo ${git_status_files[$i]:0:2}
+done
