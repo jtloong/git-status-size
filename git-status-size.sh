6216d0bdd45d4a0a4285d6937d0af61d34ee3e15
jtloong <joshua.t.loong@gmail.com>
2018-10-22 20:25:27 -0400

    Mostly finished the output format

--- git-status-size.sh
+++ git-status-size.sh
@@ -1,11 +1,49 @@
 #!/bin/bash
 
+function c () {
+    local a
+    (( $# > 0 )) && a="$@" || read -r -p "calc: " a
+    bc -l <<< "$a"
+}
+function round()
+{
+  echo $(printf %.$2f $(echo "scale=$2;(((10^$2)*$1)+0.5)/(10^$2)" | bc))
+};
+
+function get_size (){
+  var=$(stat -c%s "$2")
+  mb=$(bc <<< 'scale=2; '$var'/1000000')
+
+  #Colors
+
+  RED=`tput setaf 1`
+  GREEN=`tput setaf 2`
+  NC=`tput sgr0`
+  # RED='\033[0;31m'
+  # GREEN='\033[0;32m'
+  # NC='\033[0m'
+
+  if [[ $1 == "M" ]]
+  then
+    echo "${RED}Modified${NC}: $2 - $mb mb"
+  fi
+  if [[ $1 == "A" ]]
+  then
+    echo "${GREEN}Added${NC}: $2 - $mb mb"
+  fi
+  if [[ $1 == "??" ]]
+  then
+    echo "${RED}Untracked${NC}: $2 - $mb mb"
+  fi
+}
+
+echo "----"
 mapfile -t git_status_files < <( git status -s )
 
 tLen=${#git_status_files[@]}
 
 for (( i=0; i<${tLen}; i++ ));
 do
-  if
-  echo ${git_status_files[$i]:0:2}
+  get_size ${git_status_files[$i]}
 done
+echo "----"
